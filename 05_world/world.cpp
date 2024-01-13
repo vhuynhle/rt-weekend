3e543c75afb4f6ff883bb9fa18f2edf3cce8b2f3
Van Huynh Le <v.h.le@protonmail.com>
2024-01-13 11:24:42 +0100

    Split camera logic to a class

--- 05_world/world.cpp
+++ 05_world/world.cpp
@@ -1,81 +1,29 @@
-#include "color.h"
-#include "hittable.h"
+#include "camera.h"
 #include "hittable_list.h"
-#include "interval.h"
-#include "ray.hpp"
 #include "sphere.h"
 #include "vec3.h"
 
-#include <concepts>
 #include <cstddef>
-#include <cstdint>
-#include <format>
-#include <iostream>
-#include <limits>
 #include <memory>
-#include <optional>
-#include <type_traits>
 
-// image
+namespace {
+
 constexpr double aspect_ratio { 16.0 / 9.0 };
-constexpr std::int32_t image_width { 1024 };
-constexpr std::int32_t image_height { static_cast<std::int32_t>(image_width / aspect_ratio) };
-
-// camera
-constexpr double focal_length { 1 };
-constexpr double viewport_height { 2.0 };
-constexpr double viewport_width { viewport_height * static_cast<double>(image_width)
-                                  / image_height };
+constexpr std::size_t image_width { 512 };
 const point3d camera_center { 0, 0, 0 };
-
-template <std::floating_point T>
-color<T> ray_color(const hittable<T>& obj, const ray<T>& r) {
-    const std::optional<hit_record<T>> may_hit_obj { obj.hit(r, interval<T>::nonnegative) };
-
-    if (may_hit_obj) {
-        const vec3<T> normal { may_hit_obj->normal };
-        return color<T> { T(0.5) * normal.x() + T(0.5), T(0.5) * normal.y() + T(0.5),
-                          T(0.5) * normal.z() + T(0.5) };
-    }
-    const vec3<T> unit_direction { unit_vector(r.direction()) };
-    const T a { T(0.5) * (unit_direction.y() + T(1)) };
-
-    return (T(1) - a) * color<T> { T(1), T(1), T(1) } + a * color<T> { T(0.5), T(0.7), T(1) };
+constexpr double focal_length { 1.0 };
+constexpr double viewport_height { 2.0 };
 }
 
 int main() {
-    const vec3d viewport_u { viewport_width, 0, 0 };
-    const vec3d viewport_v { 0, -viewport_height, 0 };
-    const vec3d pixel_delta_u { viewport_u / static_cast<double>(image_width) };
-    const vec3d pixel_delta_v { viewport_v / static_cast<double>(image_height) };
+    camera<double> camera { aspect_ratio, image_width, camera_center, focal_length,
+                            viewport_height };
 
-    // The viewport is
-    //  + focal_length away from the camera in the -z direction
-    //  + to the left of the camera
-    //  + to the top of the camera
-    const point3d viewport_upper_left { camera_center - vec3d(0, 0, focal_length) - viewport_u / 2.0
-                                        - viewport_v / 2.0 };
-
-    // The center of the pixel (0, 0) is half a pixel away from the corner
-    const point3d pixel00_loc { viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v) };
-
-    // World
     hittable_list<double> world;
     world.add(std::make_shared<sphere<double>>(point3d { 0, 0, -1 }, 0.5));      // small ball
     world.add(std::make_shared<sphere<double>>(point3d { 0, -100.5, -1 }, 100)); // earth
 
-    // Render
-    std::cout << std::format("P3\n{} {}\n255\n", image_width, image_height);
-    for (std::size_t row { 0 }; row < image_height; ++row) {
-        for (std::size_t col { 0 }; col < image_width; ++col) {
-            const point3d pixel_center { pixel00_loc + (static_cast<double>(col) * pixel_delta_u)
-                                         + (static_cast<double>(row) * pixel_delta_v) };
-            const vec3d ray_direction { pixel_center - camera_center };
-            const rayd r { camera_center, ray_direction };
-            const colord pixel_color { ray_color(world, r) };
-            write_color(std::cout, pixel_color);
-        }
-    }
+    camera.render(world);
 
     return 0;
 }
---
+++ include/camera.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "color.h"
+#include "hittable.h"
+#include "ray.hpp"
+#include "vec3.h"
+
+#include <concepts>
+#include <cstddef>
+#include <iostream>
+
+template <std::floating_point T>
+class camera {
+public:
+    camera(const T aspect_ratio_, std::size_t image_width_, point3<T> center_, T focal_length_,
+           T viewport_height_)
+        : aspect_ratio { aspect_ratio_}
+        , image_width { image_width_}
+        , image_height { static_cast<std::size_t>(image_width / aspect_ratio) }
+        , center { center_ }
+        , focal_length { focal_length_ }
+        , viewport_height { viewport_height_ }
+        , viewport_width { viewport_height * static_cast<T>(image_width)
+                           / static_cast<T>(image_height) }
+        , viewport_u { viewport_width, 0, 0 }
+        , viewport_v { 0, -viewport_height, 0 }
+        , pixel_delta_u{viewport_u / static_cast<T>(image_width)}
+        , pixel_delta_v{viewport_v / static_cast<T>(image_height)}
+        , viewport_upper_left{center - vec3<T>{0, 0, focal_length} - viewport_u / T(2) - viewport_v / T(2)}
+        , pixel00_loc{viewport_upper_left + T(0.5) * (pixel_delta_u + pixel_delta_v)} { }
+
+    void render(const hittable<T>& world) const {
+        std::cout << std::format("P3\n{} {}\n255\n", image_width, image_height);
+        for (std::size_t row { 0 }; row < image_height; ++row) {
+            for (std::size_t col { 0 }; col < image_width; ++col) {
+                const point3d pixel_center { pixel00_loc
+                                             + (static_cast<double>(col) * pixel_delta_u)
+                                             + (static_cast<double>(row) * pixel_delta_v) };
+                const vec3d ray_direction { pixel_center - center };
+                const rayd r { center, ray_direction };
+                const colord pixel_color { ray_color(world, r) };
+                write_color(std::cout, pixel_color);
+            }
+        }
+    }
+
+private:
+    const T aspect_ratio;
+    const std::size_t image_width;
+    const std::size_t image_height;
+    const point3<T> center;
+    const T focal_length;
+    const T viewport_height;
+    const T viewport_width;
+    const vec3<T> viewport_u;
+    const vec3<T> viewport_v;
+    const vec3<T> pixel_delta_u;
+    const vec3<T> pixel_delta_v;
+    const point3<T> viewport_upper_left;
+    const point3<T> pixel00_loc;
+
+    color<T> ray_color(const hittable<T>& obj, const ray<T>& r) const {
+        const std::optional<hit_record<T>> may_hit_obj { obj.hit(r, interval<T>::nonnegative) };
+
+        if (may_hit_obj) {
+            const vec3<T> normal { may_hit_obj->normal };
+            return color<T> { T(0.5) * normal.x() + T(0.5), T(0.5) * normal.y() + T(0.5),
+                              T(0.5) * normal.z() + T(0.5) };
+        }
+        const vec3<T> unit_direction { unit_vector(r.direction()) };
+        const T a { T(0.5) * (unit_direction.y() + T(1)) };
+
+        return (T(1) - a) * color<T> { T(1), T(1), T(1) } + a * color<T> { T(0.5), T(0.7), T(1) };
+    }
+};
